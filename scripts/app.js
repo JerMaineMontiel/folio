12770edf2a462f5d3368168f2aeba179592f0f7b
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2016-09-01 22:46:31 -0500

    changed up photos dir; started on design page

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,39 @@
+var numPortraits = 19;
+var numDFW = 7;
+
 var folioApp = angular.module("folioApp", ['ngRoute']);
 
 folioApp.config(function($routeProvider){
   //route for resume
   $routeProvider.when('/resume', {
-    templateUrl: 'templates/resume.html'
+    templateUrl: 'templates/resume.html',
+    controller: 'mainController'
   })
   //route for the photo folio
-  .when('/photos', {
-    templateUrl: 'templates/photos.html'
+  .when('/photos/', {
+    templateUrl: 'templates/photos.html',
+    controller: 'mainController'
   })
   //route for the design page
   .when('/design', {
-    templateUrl: 'templates/design.html'
+    templateUrl: 'templates/design.html',
+    controller: 'mainController'
   });
 });
 
 folioApp.controller('mainController', function($scope){
-    $scope.photos = [];
-    
-    for(x = 20; x > 0; x--)
-      {
-        $scope.photos[20-x] = "assets/photos/" + x + ".jpg";
-      }
+
+  $scope.portraits = [];
+  $scope.dfwphotos = [];
+  $scope.designProj = designProjects;
+  
+  for(x = numPortraits; x > 0; x--)
+    {
+      $scope.portraits[numPortraits-x] = "assets/photos/portraits/" + x + ".jpg";
+    }
+
+  for(y = numDFW; y > 0; y--)
+    {
+      $scope.dfwphotos[numDFW-y] = "assets/photos/dfw/" + y + ".jpg";
+    }
 });
---
+++ scripts/designproj.js
@@ -0,0 +1,21 @@
+var designProjects = [
+  {
+    title: "personal brand refresh",
+    caption: "A look at the refresh of my site and brand identity.",
+    imageUrl: "",
+    clickthrough: "" 
+  }, 
+  {
+    title: "Branding for Obiant, Inc.",
+    caption: "Breaking down a brand identity project for a small NYC startup.",
+    imageUrl: "",
+    clickthrough: ""
+  },
+    
+  {
+    title: "$(datcode) branding and initial site design",
+    caption: "Branding for $(datcode) as well as launch promo material.",
+    imageUrl: "",
+    clickthrough: ""
+  }
+];

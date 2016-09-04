9a28b79e196f34b8ff3eb68ed5cf11521fa168bb
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2016-09-03 22:28:50 -0500

    other files

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,29 @@
+var jquery = require('jquery');
+var angular = require('angular');
+var animate = require('angular-animate');
+var route = require('angular-route');
+var designProjects = require('./design.json');
+
 var numPortraits = 19;
 var numDFW = 7;
 
-var folioApp = angular.module("folioApp", ['ngRoute']);
+var folioApp = angular.module("folioApp", ['ngRoute', 'ngAnimate']);
 
-folioApp.config(function($routeProvider){
-  //route for resume
-  $routeProvider.when('/resume', {
-    templateUrl: 'templates/resume.html',
-    controller: 'mainController'
+folioApp.config(function ($routeProvider) {
+  //route for home
+  $routeProvider.when('/', {
+    templateUrl: 'templates/home.html'
+  //route for the resume
+  }).when('/resume', {
+    templateUrl: 'templates/resume.html'
   })
   //route for the photo folio
-  .when('/photos/', {
-    templateUrl: 'templates/photos.html',
-    controller: 'mainController'
+  .when('/photos', {
+    templateUrl: 'templates/photos.html'
   })
   //route for the design page
   .when('/design', {
-    templateUrl: 'templates/design.html',
-    controller: 'mainController'
+    templateUrl: 'templates/design.html'
   });
 });
 
@@ -27,13 +33,17 @@ folioApp.controller('mainController', function($scope){
   $scope.dfwphotos = [];
   $scope.designProj = designProjects;
   
-  for(x = numPortraits; x > 0; x--)
+  var x = 1;
+  while( x <= numPortraits )
     {
-      $scope.portraits[numPortraits-x] = "assets/photos/portraits/" + x + ".jpg";
+      $scope.portraits[x-1] = "assets/photos/portraits/" + x + ".jpg";
+      x++;
     }
 
-  for(y = numDFW; y > 0; y--)
+  var y = 1;
+  while( y <= numDFW )
     {
-      $scope.dfwphotos[numDFW-y] = "assets/photos/dfw/" + y + ".jpg";
+      $scope.dfwphotos[y-1] = "assets/photos/dfw/" + y + ".jpg";
+      y++;
     }
 });

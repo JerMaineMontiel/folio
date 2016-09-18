60a8d166b360f372d85d0a63c43d39283d11b041
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2016-09-17 19:43:07 -0500

    rework. wasn't feeling it.
    made it all one page, with anchor scrolling.

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,49 @@
 var jquery = require('jquery');
 var angular = require('angular');
-var animate = require('angular-animate');
-var route = require('angular-route');
 var designProjects = require('./design.json');
 
 var numPortraits = 19;
 var numDFW = 7;
 
-var folioApp = angular.module("folioApp", ['ngRoute', 'ngAnimate']);
-
-folioApp.config(function ($routeProvider) {
-  //route for home
-  $routeProvider.when('/', {
-    templateUrl: 'templates/home.html'
-  //route for the resume
-  }).when('/resume', {
-    templateUrl: 'templates/resume.html'
-  })
-  //route for the photo folio
-  .when('/photos', {
-    templateUrl: 'templates/photos.html'
-  })
-  //route for the design page
-  .when('/design', {
-    templateUrl: 'templates/design.html'
-  });
-});
+var folioApp = angular.module("folioApp", []);
 
 folioApp.controller('mainController', function($scope){
 
-  $scope.portraits = [];
-  $scope.dfwphotos = [];
-  $scope.designProj = designProjects;
-  
-  var x = 1;
-  while( x <= numPortraits )
-    {
-      $scope.portraits[x-1] = "assets/photos/portraits/" + x + ".jpg";
-      x++;
+    $scope.portraits = [];
+    $scope.dfwphotos = [];
+    $scope.designProj = designProjects;
+
+    var x = 1;
+    while( x <= numPortraits )
+      {
+        $scope.portraits[x-1] = "assets/photos/portraits/" + x + ".jpg";
+        x++;
+      }
+
+    var y = 1;
+    while( y <= numDFW )
+      {
+        $scope.dfwphotos[y-1] = "assets/photos/dfw/" + y + ".jpg";
+        y++;
+      }
+});
+
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
     }
 
-  var y = 1;
-  while( y <= numDFW )
-    {
-      $scope.dfwphotos[y-1] = "assets/photos/dfw/" + y + ".jpg";
-      y++;
-    }
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $(id).offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
 });
